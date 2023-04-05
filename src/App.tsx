ba3ddd6d6e5425f2316c376ba204a5c9baef2456
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-06 04:11:44 +0500

    👌 IMPROVE: basic project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Grid, GridItem } from '@chakra-ui/react';
+
 function App() {
-  return <h1>Rethink Fe</h1>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"aside nav" "aside main"`
+      }}
+    >
+      <GridItem area='nav'>Nav</GridItem>
+      <GridItem area='aside'>Aside</GridItem>
+      <GridItem area='main'>Main</GridItem>
+    </Grid>
+  );
 }
 
 export default App;
