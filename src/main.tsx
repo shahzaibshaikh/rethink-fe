a4076c3a099e0ffc47635483b03daadcd82993a2
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-06 04:18:59 +0500

    👌 IMPROVE: modified color theme gray palette

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
