1ad4004d203935e651016cff4ab203e5369c9b41
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-06 06:08:49 +0500

    👌 IMPROVE: redefined layout and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import { Grid, GridItem, Show } from '@chakra-ui/react';
+import { Box, Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
   return (
     <Grid
-      gridTemplateAreas={{ md: `"aside center main"`, base: `"center main"` }}
-      gridTemplateColumns={{ md: '200px 300px auto', base: 'repeat(3, 1fr)' }}
+      gridTemplateAreas={{ lg: `"aside center main"`, base: `"center main"` }}
+      gridTemplateColumns={{ lg: '250px 300px auto', base: '1fr 2fr' }}
     >
-      <Show above='md'>
-        <GridItem>Aside</GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' background='gray.800' height='100vh'>
+          Aside
+        </GridItem>
       </Show>
-      <GridItem>Center</GridItem>
-      <GridItem>Main</GridItem>
+      <GridItem area='center' background='gray.700' height='100vh'>
+        Center
+      </GridItem>
+      <GridItem area='main' background='gray.700' height='100vh' padding={3}>
+        <Box background='gray.200' color='gray.900' borderRadius={10} height='100%'>
+          Main
+        </Box>
+      </GridItem>
     </Grid>
   );
 }
