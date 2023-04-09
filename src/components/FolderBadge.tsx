b3f6162bed40a82572885c23cc1f88397a78dd37
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-10 04:41:32 +0500

    👌 IMPROVE: made folder badge invisible

--- src/components/FolderBadge.tsx
+++ src/components/FolderBadge.tsx
@@ -6,7 +6,13 @@ interface FolderBadgeProps {
 
 function FolderBadge({ folder_name }: FolderBadgeProps) {
   return (
-    <Badge borderRadius={10} padding='2px 6px' fontSize={10} colorScheme='purple'>
+    <Badge
+      borderRadius={10}
+      padding='2px 6px'
+      fontSize={10}
+      colorScheme='purple'
+      visibility={folder_name ? 'visible' : 'hidden'}
+    >
       {folder_name}
     </Badge>
   );
