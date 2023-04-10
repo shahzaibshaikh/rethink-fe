b9b413def0c6138a3eee2f91f875a3367f5566d5
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-10 05:02:49 +0500

    👌 IMPROVE:  added loader on note detail

--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, HStack, Input, InputGroup } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Spinner } from '@chakra-ui/react';
 import { NoteStateOne } from '../interfaces/NoteInterface';
 import apiClient from '../services/apiClient';
 import { setData, setError, setLoading } from '../store/slices/noteDetailSlice';
@@ -54,50 +55,58 @@ function MainForm() {
   }
 
   return (
-    <Box mt={3}>
-      <HStack width='100%' justifyContent='space-between' gap={2}>
-        <form style={{ width: '100%' }}>
-          <InputGroup>
-            <Input
-              value={data?.title ?? ''}
-              placeholder='Title'
-              onChange={event =>
-                dispatch(setData({ ...data, title: event.target.value }))
-              }
-              border='none'
-              background='gray.700'
-              fontWeight={700}
-            />
-          </InputGroup>
-        </form>
-        <NoteOptionsIcon />
-      </HStack>
-      <MainFormMeta
-        folder_name={data?.folder?.name ?? 'All notes'}
-        date={data?.updated_at ?? now.toString()}
-      />
-      <RichText />
-      <HStack
-        justifyContent={isExistingNote ? 'space-between' : 'flex-end'}
-        alignItems='center'
-        mt={3}
-      >
-        {isExistingNote && (
-          <Button fontSize='sm' colorScheme='red' variant='outline'>
-            Delete note
-          </Button>
-        )}
-        <Button
-          onClick={handleSubmit}
-          fontSize='sm'
-          color='white'
-          variant='solid'
-          bg='purple.600'
-          _hover={{ bg: 'gray.700', color: 'gray.100' }}
-        >
-          Save note
-        </Button>
-      </HStack>
+    <Box mt={3} height='100%'>
+      {loading ? (
+        <Box display='grid' placeItems='center' height='100%'>
+          <Spinner size='xl' color='purple.500' mb={6} mt={4} />
+        </Box>
+      ) : (
+        <>
+          <HStack width='100%' justifyContent='space-between' gap={2}>
+            <form style={{ width: '100%' }}>
+              <InputGroup>
+                <Input
+                  value={data?.title ?? ''}
+                  placeholder='Title'
+                  onChange={event =>
+                    dispatch(setData({ ...data, title: event.target.value }))
+                  }
+                  border='none'
+                  background='gray.700'
+                  fontWeight={700}
+                />
+              </InputGroup>
+            </form>
+            <NoteOptionsIcon />
+          </HStack>
+          <MainFormMeta
+            folder_name={data?.folder?.name ?? 'All notes'}
+            date={data?.updated_at ?? now.toString()}
+          />
+          <RichText />
+          <HStack
+            justifyContent={isExistingNote ? 'space-between' : 'flex-end'}
+            alignItems='center'
+            mt={3}
+          >
+            {isExistingNote && (
+              <Button fontSize='sm' colorScheme='red' variant='outline'>
+                Delete note
+              </Button>
+            )}
+            <Button
+              onClick={handleSubmit}
+              fontSize='sm'
+              color='white'
+              variant='solid'
+              bg='purple.600'
+              _hover={{ bg: 'gray.700', color: 'gray.100' }}
+            >
+              Save note
+            </Button>
+          </HStack>
+        </>
+      )}
     </Box>
   );
 }
