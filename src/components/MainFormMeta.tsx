41fca3476edc48fbb0e2568f9d7e6f4a035bbfdb
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-12 19:47:04 +0500

    👌 IMPROVE: added folder change button

--- src/components/MainFormMeta.tsx
+++ src/components/MainFormMeta.tsx
@@ -1,5 +1,14 @@
-import { Box, HStack, Text } from '@chakra-ui/react';
-import { BsFillCalendar3WeekFill, BsFillFolderFill } from 'react-icons/bs';
+import {
+  Box,
+  Button,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text
+} from '@chakra-ui/react';
+import { BsChevronDown, BsFillCalendar3WeekFill, BsFillFolderFill } from 'react-icons/bs';
 import formatDate from '../utilities/dateFormatter';
 import HorizontalLine from './HorizontalLine';
 
@@ -33,7 +42,21 @@ function MainFormMeta({ date, folder_name }: MainFormMetaProps) {
             <Text>Folder</Text>
           </Box>
         </HStack>
-        <Text color='white'>{folder_name}</Text>
+
+        <Menu isLazy>
+          <MenuButton fontSize='13px' fontWeight={600} color='white'>
+            <HStack>
+              <Text> Open menu</Text>
+              <BsChevronDown />
+            </HStack>
+          </MenuButton>
+          <MenuList zIndex={10} color='white'>
+            {/* MenuItems are not rendered unless Menu is open */}
+            <MenuItem>New Window</MenuItem>
+            <MenuItem>Open Closed Tab</MenuItem>
+            <MenuItem>Open File</MenuItem>
+          </MenuList>
+        </Menu>
       </HStack>
       <HorizontalLine />
     </Box>
